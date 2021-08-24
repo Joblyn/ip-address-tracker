be005ed7e27498249d59bf60614b1650f0095ef4
Joblyn <oaikhenahjob@gmail.com>
2021-08-24 18:07:04 +0100

    initialsed map with leafletJs

--- src/App.js
+++ src/App.js
@@ -7,17 +7,6 @@ function App() {
     <div className="App">
       <HeaderContainer />
       <MapSection />
-      {/* IP Address Tracker Search for any IP address or domain IP Address Location
-      Timezone UTC */}
-      {/* <!-- add offset value dynamically using the API --> */}
-      {/* ISP
-      <div class="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="www.joblyn.netlify.app">Joblyn</a>.
-      </div> */}
     </div>
   );
 }
--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { Container } from './styles';
+import React from "react";
+import { Container } from "./styles";
+import L from "leaflet";
 
 export default function Map({ ...restProps }) {
-  return (
-    <Container { ...restProps }></Container>
+  var map = L.map("mapid").setView([51.505, -0.09], 13);
   
-  )
-};
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+  return <Container {...restProps} id="mapid"></Container>;
+}
